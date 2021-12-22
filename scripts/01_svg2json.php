f4a33e80b460885c1e6df712e7f3db65e527d8e2
kiang <kiange@gmail.com>
2021-12-22 23:03:10 +0800

    add zone to json files

--- scripts/01_svg2json.php
+++ scripts/01_svg2json.php
@@ -2,339 +2,395 @@
 $id2meta = [
     5 => [
         'seat' => 51,
-        'name' => '許至樁',
+        'name' => '許至椿',
         'party' => '國',
-        'status' => 3
+        'status' => 3,
+        'zone' => 8,
     ],
     7 => [
         'seat' => 41,
         'name' => '呂維胤',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 9,
     ],
     9 => [
         'seat' => 31,
         'name' => '林阳乙',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 7,
     ],
     11 => [
         'seat' => 21,
         'name' => '李文俊',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 5,
     ],
     13 => [
         'seat' => 11,
         'name' => '劉米山',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 1,
     ],
     15 => [
         'seat' => 1,
         'name' => '郭信良',
         'party' => '無',
-        'status' => 0
+        'status' => 0,
+        'zone' => 6,
     ],
     17 => [
         'seat' => 52,
         'name' => '張世賢',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 1,
     ],
     19 => [
         'seat' => 42,
         'name' => '林志展',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 5,
     ],
     21 => [
         'seat' => 32,
         'name' => '周奕齊',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 4,
     ],
     23 => [
         'seat' => 22,
         'name' => '曾培雅',
         'party' => '國',
-        'status' => 3
+        'status' => 3,
+        'zone' => 10,
     ],
     25 => [
         'seat' => 12,
         'name' => '蔡蘇秋金',
         'party' => '民',
-        'status' => 2
+        'status' => 2,
+        'zone' => 2,
     ],
     27 => [
         'seat' => 2,
         'name' => '林炳利',
         'party' => '無',
-        'status' => 1
+        'status' => 1,
+        'zone' => 6,
     ],
     29 => [
         'seat' => 53,
         'name' => '方一峰',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 2,
     ],
     31 => [
         'seat' => 43,
         'name' => '鄭佳欣',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 11,
     ],
     33 => [
         'seat' => 33,
         'name' => '林志聰',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 5,
     ],
     35 => [
         'seat' => 23,
         'name' => '林美燕',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 9,
     ],
     37 => [
         'seat' => 13,
         'name' => '陳昆和',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 2,
     ],
     39 => [
         'seat' => 3,
         'name' => '趙昆原',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 1,
     ],
     41 => [
         'seat' => 54,
         'name' => '蔡育輝',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 1,
     ],
     43 => [
         'seat' => 44,
         'name' => '蔡旺詮',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 10,
     ],
     45 => [
         'seat' => 34,
         'name' => '陳怡珍',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 8,
     ],
     47 => [
         'seat' => 24,
         'name' => '林燕祝',
         'party' => '國',
-        'status' => 3
+        'status' => 3,
+        'zone' => 7,
     ],
     49 => [
         'seat' => 14,
         'name' => '郭清華',
         'party' => '民',
-        'status' => 2
+        'status' => 2,
+        'zone' => 6,
     ],
     51 => [
         'seat' => 4,
-        'name' => '谷暮·哈就',
+        'name' => '谷暮．哈就',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 13,
     ],
     53 => [
         'seat' => 55,
         'name' => '蔡淑惠',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 9,
     ],
     55 => [
         'seat' => 45,
         'name' => '吳禹寰',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 11,
     ],
     57 => [
         'seat' => 35,
         'name' => '王錦德',
         'party' => '民',
-        'status' => 2
+        'status' => 2,
+        'zone' => 6,
     ],
     59 => [
         'seat' => 25,
         'name' => '杜素吟',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 11,
     ],
     61 => [
         'seat' => 15,
         'name' => '蔡秋蘭',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 2,
     ],
     63 => [
         'seat' => 5,
         'name' => '盧崑福',
         'party' => '國',
-        'status' => 1
+        'status' => 1,
+        'zone' => 9,
     ],
     65 => [
         'seat' => 56,
         'name' => '尤榮智',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 3,
     ],
     67 => [
         'seat' => 46,
         'name' => '吳通龍',
         'party' => '民',
-        'status' => 2
+        'status' => 2,
+        'zone' => 3,
     ],
     71 => [
         'seat' => 26,
         'name' => '黃麗招',
         'party' => '無',
-        'status' => 3
+        'status' => 3,
+        'zone' => 6,
     ],
     73 => [
         'seat' => 16,
         'name' => '李啟維',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 9,
     ],
     75 => [
         'seat' => 6,
         'name' => '曾信凱',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 10,
     ],
     77 => [
         'seat' => 57,
         'name' => '洪玉鳳',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 8,
     ],
     79 => [
         'seat' => 47,
         'name' => '周麗津',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 9,
     ],
     81 => [
         'seat' => 37,
         'name' => '楊中成',
         'party' => '台',
-        'status' => 3
+        'status' => 3,
+        'zone' => 7,
     ],
     83 => [
         'seat' => 27,
         'name' => '邱莉莉',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 8,
     ],
     85 => [
         'seat' => 17,
         'name' => '郭鴻儀',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 11,
     ],
     87 => [
         'seat' => 7,
         'name' => '林易瑩',
         'party' => '時',
-        'status' => 3
+        'status' => 3,
+        'zone' => 7,
     ],
     91 => [
         'seat' => 48,
         'name' => '李中岑',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 6,
     ],
     93 => [
         'seat' => 38,
         'name' => '謝財旺',
         'party' => '無',
-        'status' => 1
+        'status' => 1,
+        'zone' => 2,
     ],
     95 => [
         'seat' => 28,
         'name' => '李偉智',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 5,
     ],
     97 => [
         'seat' => 18,
         'name' => '謝龍介',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 8,
     ],
     99 => [
         'seat' => 8,
         'name' => '許又仁',
         'party' => '無',
-        'status' => 2
+        'status' => 2,
+        'zone' => 11,
     ],
     103 => [
         'seat' => 49,
         'name' => '李宗翰',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 1,
     ],
     105 => [
         'seat' => 39,
         'name' => '陳秋宏',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 3,
     ],
     107 => [
         'seat' => 29,
         'name' => '穎艾達利',
         'party' => '無',
-        'status' => 3
+        'status' => 3,
+        'zone' => 12,
     ],
     109 => [
         'seat' => 19,
         'name' => '王家貞',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 10,
     ],
     111 => [
         'seat' => 9,
         'name' => '陳碧玉',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 5,
     ],
     115 => [
         'seat' => 50,
         'name' => '蔡筱薇',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 10,
     ],
     117 => [
         'seat' => 40,
         'name' => '沈家鳳',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 1,
     ],
     119 => [
         'seat' => 30,
         'name' => '李鎮國',
         'party' => '國',
-        'status' => 2
+        'status' => 2,
+        'zone' => 7,
     ],
     121 => [
         'seat' => 20,
         'name' => '沈震東',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 8,
     ],
     123 => [
         'seat' => 10,
         'name' => '陳秋萍',
         'party' => '民',
-        'status' => 3
+        'status' => 3,
+        'zone' => 7,
     ],
 ];
 $color2status = [
